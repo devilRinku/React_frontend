9f66eccb376e68ed37fac15a0fbde185b63f8c09
bapun nath <rinkusahoo181@gmail.com>
2023-11-02 08:34:39 +0530

    Your commit message here

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
-function Card(props){
-  return(
-<>
+function Card(props) {
+  return (
     <div className="cards">
       <div className="card">
-        <img src="" alt={props.imgsc} className="card_img" />
+        <img src={props.imgSrc} alt={props.sname} className="card_img" />
         <div className="card_info">
-          <span className="card_categories"> {props.titel}</span>
-          <h3 className="card_titel">{props.sname}</h3>
-          <a href="{props.link}" target="_blank">
+          <span className="card_categories">{props.title}</span>
+          <h3 className="card_title">{props.sname}</h3>
+          <a href={props.link} target="_blank">
             <button>Watch now</button>
           </a>
         </div>
       </div>
     </div>
-  </>);
+  );
 }
+
 export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from './Card';
+import Sdata from './Sdata';
+console.log(Sdata)
 ReactDOM.render(
-  <>
-  <Card imgsc="https://m.media-amazon.com/images/M/MV5BOTY0ZjliOGUtMDFiZS00YzcwLWFiZWYtMzg4MWZkMWIwYzYwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg"
-  sname="shadow and bone"
-  titel="A Netflix Original Series"
-  link="https://www.netflix.com/watch/80236160?trackId=14315607"/>
-  </>,document.getElementById('root')
+  <div>
+    <Card
+      imgSrc={Sdata[0].imgSrc}
+      sname={Sdata[0].sname}
+      title={Sdata[0].title}
+      link={Sdata[0].link}
+    />
+     <Card
+      imgSrc={Sdata[1].imgSrc}
+      sname={Sdata[1].sname}
+      title={Sdata[1].title}
+      link={Sdata[1].link}
+    />
+     <Card
+      imgSrc={Sdata[2].imgSrc}
+      sname={Sdata[2].sname}
+      title={Sdata[2].title}
+      link={Sdata[2].link}
+    />
+     <Card
+      imgSrc={Sdata[3].imgSrc}
+      sname={Sdata[3].sname}
+      title={Sdata[3].title}
+      link={Sdata[3].link}
+    />
+  </div>,
+  document.getElementById('root')
 );
