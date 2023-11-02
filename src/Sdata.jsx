797742ad5088e680622d5568a2f2eb0addb4adaf
bapun nath <rinkusahoo181@gmail.com>
2023-11-02 08:36:51 +0530

    Add src/Sdata.jsx file

---
+++ src/Sdata.jsx
@@ -0,0 +1,23 @@
+const Sdata = [
+  {
+    imageSrc: "https://m.media-amazon.com/images/M/MV5BOTY0ZjliOGUtMDFiZS00YzcwLWFiZWYtMzg4MWZkMWIwYzYwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    sname: "Shadow and Bone",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/watch/80236160?trackId=14315607",
+  },{
+    imageSrc: "https://m.media-amazon.com/images/M/MV5BOTY0ZjliOGUtMDFiZS00YzcwLWFiZWYtMzg4MWZkMWIwYzYwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    sname: "Shadow and Bone",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/watch/80236160?trackId=14315607",
+},{
+    imageSrc: "https://m.media-amazon.com/images/M/MV5BOTY0ZjliOGUtMDFiZS00YzcwLWFiZWYtMzg4MWZkMWIwYzYwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    sname: "Shadow and Bone",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/watch/80236160?trackId=14315607",
+},{
+    imageSrc: "https://m.media-amazon.com/images/M/MV5BOTY0ZjliOGUtMDFiZS00YzcwLWFiZWYtMzg4MWZkMWIwYzYwXkEyXkFqcGdeQXVyMTUzMTg2ODkz._V1_.jpg",
+    sname: "Shadow and Bone",
+    title: "A Netflix Original Series",
+    link: "https://www.netflix.com/watch/80236160?trackId=14315607",
+},];
+export default Sdata;
